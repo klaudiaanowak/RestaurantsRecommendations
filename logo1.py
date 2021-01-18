5f716847a1435aac2572355f8c6688029d48652e
vvera98 <76257475+vvera98@users.noreply.github.com>
2021-01-18 15:00:57 +0100

    Add files via upload

--- logo1.py
+++ logo1.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import sys
-import os
 import sqlite3
+from tkinter import messagebox
 
 
 
@@ -12,7 +12,7 @@ def register_user():
     conn = sqlite3.connect('Recommendations.db')
     c = conn.cursor()
 
-    c.execute('INSERT INTO  users (login, haslo, email, tel) VALUES(?,?,?,?)',(login.get(), haslo.get(), email.get(), tel.get()))
+    c.execute('INSERT INTO  users (login, haslo, email, telefon) VALUES(?,?,?,?)',(login.get(), haslo.get(), email.get(), tel.get()))
 
     conn.commit() 
     conn.close()
@@ -22,7 +22,9 @@ def register_user():
     email_entry.delete(0, END)
     tel_entry.delete(0, END)
 
-    screen7 = Toplevel(screen)
+    screen1.destroy()
+    global screen7
+    screen7 = Tk()
     screen7.title('Rejestracja')
     screen7.geometry('200x200')
     Label(screen7, text = 'Zostałeś zarejestrowany!', fg='green', font=('Calibri, 11')).pack()
@@ -31,7 +33,9 @@ def register_user():
 
 
 def rejestracja():
-    screen1 = Toplevel(screen)
+    screen.destroy()
+    global screen1
+    screen1 = Tk()
     screen1.title('Rejestracja')
     screen1.geometry('400x400')
 
@@ -69,7 +73,9 @@ def rejestracja():
     Button(screen1, text = 'Wyjście', height = '1', width = '10', command = koniec).pack()
 
 def udanelogowanie():
-    screen3 = Toplevel(screen)
+    screen7.destroy()
+    global screen3
+    screen3 = Tk()
     screen3.title('Logowanie')
     screen3.geometry('400x400')
 
@@ -77,12 +83,58 @@ def udanelogowanie():
     Label(screen3, text = '').pack()
     Button(screen3, text = 'Sprawdź rekomendacje restauracji', height = '2', width = '30').pack()
     Label(screen3, text = '').pack()
-    Button(screen3, text = 'Dodaj opinie o restauracji', height = '2', width = '30', command = opinia).pack()
+    Button(screen3, text = 'Dodaj opinie o restauracji', height = '2', width = '30', command = wybor).pack()
     Label(screen3, text = '').pack()
     Button(screen3, text = 'Wyjście', height = '2', width = '30', command = koniec).pack()
 
+def wybierz():
+    global my_listbox
+    clicked_items = my_listbox.curselecion()
+    for item in clicked_items:
+        Label(screen9, textvariable = my_listbox.get(item))
+
+
+def wybor():
+    screen3.destroy()
+    global screen9
+    screen9 = Tk()
+    screen9.title('Wybór restauracji')
+    screen9.geometry('400x400')
+
+    Label(screen9, text = 'Wybierz restauracje, którą chcesz ocenić', font=('Calibri, 11')).pack()
+    Label(screen9, text = '').pack()
+
+
+    my_listbox= Listbox(screen9, selectmode = SINGLE)
+
+
+    
+
+    my_listbox.insert(1, 'Steakhouse')
+    my_listbox.insert(2, 'Coffee & Tea')
+    my_listbox.insert(3, 'Bars')
+    my_listbox.insert(4, 'Mexican')
+    my_listbox.insert(5, 'Specialty Food')
+    my_listbox.insert(6, 'Thai')
+    my_listbox.insert(7, 'Buffets')
+    my_listbox.insert(8, 'Burgers')
+    my_listbox.insert(9, 'Canadian (New)')
+    my_listbox.insert(10, 'Sandwiches')
+    my_listbox.insert(11, 'Chinese')
+    my_listbox.insert(12, 'Japanese')
+    my_listbox.insert(13, 'Fast Food')
+    my_listbox.pack(pady=15)
+    
+    
+
+    Button(screen9, text = 'Wybierz', height = '2', width = '30', command = wybierz).pack()
+    Button(screen9, text = 'Wyjście', height = '2', width = '30', command = koniec).pack()
+    
+
 def opinia():
-    screen4 = Toplevel(screen)
+    screen9.destroy()
+    global screen4
+    screen4 = Tk()
     screen4.title('Rekomendacje restauracji')
     screen4.geometry('400x400')
 
@@ -112,6 +164,18 @@ def opinia():
     Button(screen4, text = 'Dodaj', height = '2', width = '30', command = dodanoopinie).pack()
     Button(screen4, text = 'Wyjście', height = '2', width = '30', command = koniec).pack()
 
+    
+
+def weryfikacja():
+    conn = sqlite3.connect('Recommendations.db')
+    c = conn.cursor()
+    results= c.fetchall()
+
+
+
+    conn.commit() 
+    conn.close()
+
 def dodanoopinie():
     
     conn = sqlite3.connect('Recommendations.db')
@@ -125,8 +189,10 @@ def dodanoopinie():
     ocena_entry.delete(0, END)
     komentarz_entry.delete(0, END)
     restauracja_entry.delete(0, END)
-
-    screen8 = Toplevel(screen)
+    
+    screen4.destroy()
+    global screen8
+    screen8 = Tk()
     screen8.title('Rekomendacje restauracji')
     screen8.geometry('400x400')
 
@@ -138,11 +204,12 @@ def dodanoopinie():
     Label(screen8, text = '').pack()
     Button(screen8, text = 'Wyjście', height = '2', width = '30', command = koniec).pack()
 
-    
+
 
 def logowanie():
+    screen.destroy()
     global screen2
-    screen2 = Toplevel(screen)
+    screen2 = Tk()
     screen2.title('Logowanie')
     screen2.geometry('400x400')
 
@@ -173,9 +240,10 @@ def logowanie():
     haslo_entry1 = Entry(screen2, textvariable = haslo_verify)
     haslo_entry1.pack()
     Label(screen2, text = '').pack()
-    Button(screen2, text = 'Zaloguj się', width = 10, height = 1, command = udanelogowanie).pack()
+    Button(screen2, text = 'Zaloguj się', width = 10, height = 1, command = weryfikacja).pack()
     Button(screen2, text = 'Wyjście', height = '1', width = '10', command = koniec).pack()
 
+    
 def main_screen():
     global screen
     screen = Tk()
