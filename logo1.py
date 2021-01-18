7e4678e6e7bcbbd8a15b1940c1f859e39c7d1284
vvera98 <76257475+vvera98@users.noreply.github.com>
2021-01-18 20:24:16 +0100

    Add files via upload

--- logo1.py
+++ logo1.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import sys
 import sqlite3
-from tkinter import messagebox
+from tkinter import messagebox as ms
 
 
 
@@ -9,10 +9,10 @@ def koniec():
     sys.exit()
 def register_user():
 
-    conn = sqlite3.connect('Recommendations.db')
+    conn = sqlite3.connect('RecommendationsDB.db')
     c = conn.cursor()
 
-    c.execute('INSERT INTO  users (login, haslo, email, telefon) VALUES(?,?,?,?)',(login.get(), haslo.get(), email.get(), tel.get()))
+    c.execute('INSERT INTO  users (user_id, name, review_count, stars, haslo, email, telefon) VALUES(?,?,?,?,?,?,?)',(login.get(), imie.get(), '0', '0', haslo.get(), email.get(), tel.get()))
 
     conn.commit() 
     conn.close()
@@ -21,6 +21,7 @@ def register_user():
     haslo_entry.delete(0, END)
     email_entry.delete(0, END)
     tel_entry.delete(0, END)
+    imie_entry.delete(0, END)
 
     screen1.destroy()
     global screen7
@@ -29,7 +30,7 @@ def register_user():
     screen7.geometry('200x200')
     Label(screen7, text = 'Zostałeś zarejestrowany!', fg='green', font=('Calibri, 11')).pack()
     Label(screen7, text = '').pack()
-    Button(screen7, text = 'Dalej', width = 10, height = 1, command = udanelogowanie).pack()
+    Button(screen7, text = 'Dalej', width = 10, height = 1, command = logowanie).pack()
 
 
 def rejestracja():
@@ -43,15 +44,18 @@ def rejestracja():
     global haslo
     global email
     global tel
+    global imie
     global login_entry
     global haslo_entry
     global email_entry
     global tel_entry
+    global imie_entry
 
     email = StringVar()
     login = StringVar()
     haslo = StringVar()
     tel = StringVar()
+    imie = StringVar()
 
 
     Label(screen1, text = 'Uzupełnij dane ponizej').pack()
@@ -59,6 +63,9 @@ def rejestracja():
     Label(screen1, text = 'E-mail: ').pack()
     email_entry = Entry(screen1, textvariable = email)
     email_entry.pack()
+    Label(screen1, text = 'Imię: ').pack()
+    imie_entry = Entry(screen1, textvariable = imie)
+    imie_entry.pack()
     Label(screen1, text = 'Login: ').pack()
     login_entry = Entry(screen1, textvariable = login)
     login_entry.pack()
@@ -95,7 +102,7 @@ def wybierz():
 
 
 def wybor():
-    screen3.destroy()
+    screen11.destroy()
     global screen9
     screen9 = Tk()
     screen9.title('Wybór restauracji')
@@ -139,14 +146,10 @@ def opinia():
     screen4.geometry('400x400')
 
     global ocena
-    global komentarz
-    global restauracja
     global ocena_entry
-    global komentarz_entry
     global restauracja_entry
 
     ocena = StringVar()
-    komentarz = StringVar()
     restauracja = StringVar()
 
     Label(screen4, text = 'Dodaj opienie o restauracji', height = '2', width = '30').pack()
@@ -157,31 +160,18 @@ def opinia():
     Label(screen4, text = 'Ocena (1-5): ').pack()
     ocena_entry = Entry(screen4, textvariable = ocena)
     ocena_entry.pack()
-    Label(screen4, text = 'Komentarz: ').pack()
-    komentarz_entry = Entry(screen4, textvariable = komentarz)
-    komentarz_entry.pack()
     Label(screen4, text = '').pack()
     Button(screen4, text = 'Dodaj', height = '2', width = '30', command = dodanoopinie).pack()
     Button(screen4, text = 'Wyjście', height = '2', width = '30', command = koniec).pack()
 
-    
 
-def weryfikacja():
-    conn = sqlite3.connect('Recommendations.db')
-    c = conn.cursor()
-    results= c.fetchall()
-
-
-
-    conn.commit() 
-    conn.close()
 
 def dodanoopinie():
     
-    conn = sqlite3.connect('Recommendations.db')
+    conn = sqlite3.connect('RecommendationsDB.db')
     c = conn.cursor()
 
-    c.execute('INSERT INTO  reviews (login, nazwarestauracji, ocena, komentarz) VALUES(?,?,?,?)',(login.get(), restauracja.get(), ocena.get(), komentarz.get()))
+    c.execute("""INSERT INTO  reviews (review_id, user_id, business_id, stars, date) VALUES(?,?,?,?,?),('', login.get(), '', ocena.get(), '0')""")
 
     conn.commit() 
     conn.close()
@@ -242,6 +232,36 @@ def logowanie():
     Label(screen2, text = '').pack()
     Button(screen2, text = 'Zaloguj się', width = 10, height = 1, command = weryfikacja).pack()
     Button(screen2, text = 'Wyjście', height = '1', width = '10', command = koniec).pack()
+
+def weryfikacja():
+    with sqlite3.connect('RecommendationsDB.db') as db:
+        c = db.cursor()
+    find_user = ('SELECT * FROM users WHERE user_id = ? and haslo = ?')
+    c.execute(find_user,[(login_verify.get()),(haslo_verify.get())])
+    result = c.fetchall()
+
+    screen2.destroy()
+    global screen11
+    screen11 = Tk()
+    screen11.title('Logowanie')
+    screen11.geometry('400x400')
+
+    if result:
+        
+        Label(screen11, text = 'Jesteś zalogowany!', fg='green', font=('Calibri, 11')).pack()
+        Label(screen11, text = '').pack()
+        Button(screen11, text = 'Sprawdź rekomendacje restauracji', height = '2', width = '30').pack()
+        Label(screen11, text = '').pack()
+        Button(screen11, text = 'Dodaj opinie o restauracji', height = '2', width = '30', command = wybor).pack()
+        Label(screen11, text = '').pack()
+        Button(screen11, text = 'Wyjście', height = '2', width = '30', command = koniec).pack()
+    else:
+        
+        Label(screen11, text = 'Nie udało się zalogować!', fg='red', font=('Calibri, 11')).pack()
+        Label(screen11, text = '').pack()
+        Button(screen11, text = 'Wróć do strony początkowej', height = '2', width = '30', command = main_screen).pack()
+        Label(screen11, text = '').pack()
+        Button(screen11, text = 'Wyjście', height = '2', width = '30', command = koniec).pack()
 
     
 def main_screen():
